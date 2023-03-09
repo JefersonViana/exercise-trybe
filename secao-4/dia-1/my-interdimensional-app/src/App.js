3d53aceb0333ef869519ae97b15f0af5adfede06
jeferson-viana <jefersonv28@gmail.com>
2023-03-09 11:26:19 -0300

    exercicio de fixação realizado

--- secao-4/dia-1/my-interdimensional-app/src/App.js
+++ secao-4/dia-1/my-interdimensional-app/src/App.js
@@ -1,11 +1,44 @@
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <h1>
-      Hello world
-    </h1>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+        characters: [],
+    };
+  }
+
+  fetchCharacters = () => {
+    fetch('https://rickandmortyapi.com/api/character')
+    .then(response => response.json())
+    .then(data => {
+      this.setState({characters: data.results})
+    })
+  }
+  componentDidMount = () => {
+    this.fetchCharacters();
+  };
+  render() {
+    const { characters } = this.state;
+    return (
+      <div className="App">
+        <h1>
+          Ricky and Morty Characters:
+        </h1>
+        <div className="body">
+          {characters.map(({ name, image }, index) => {
+            return (
+              <div className="container" key={ index }>
+                <h3>{ name }</h3>
+                <img src={ image } alt={ name } />
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
